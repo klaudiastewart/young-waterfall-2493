eebe2061310fe5e9e6382b0ff49952f55b7968c3
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-12 09:50:34 -0600

    Update model tests relationships for all tables

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,5 +1,6 @@
 class StudiosController < ApplicationController
   def show
     @studio = Studio.find(params[:id])
+    binding.pry
   end
 end
--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe Studio, type: :feature do
     expect(page).to have_content(titanic.title)
     expect(page).to have_content(dumbndumber.title)
   end
+
+  it 'shows a list of actors that have acted in any of the studios movies' do
+    fox = Studio.create!(name: "Fox", location: "Hollywood")
+    titanic = fox.movies.create!(title: "Titanic", creation_year: 1996, genre: "Drama")
+    dumbndumber = fox.movies.create!(title: "Dumb", creation_year: 2003, genre: "Comedy")
+    leonardo = titanic.actors.create!(name: "Leonardo Dicaprio", age: 23, currently_working: true)
+
+    visit "/studios/#{fox.id}"
+
+    expect(page).to have_content(leonardo.name)
+  end
 end
