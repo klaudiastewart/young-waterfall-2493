231c73c178704adbe0b3185ece9d8f3a6e7f4f0e
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-12 09:26:35 -0600

    Complete us1 with tests

--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Studio, type: :feature do
     expect(page).to have_content(fox.name)
     expect(page).to have_content(fox.location)
   end
+
+  it 'shows all the titles of all the studios movies' do
+    fox = Studio.create!(name: "Fox", location: "Hollywood")
+    titanic = fox.movies.create!(title: "Titanic", creation_year: 1996, genre: "Drama")
+    dumbndumber = fox.movies.create!(title: "Dumb", creation_year: 2003, genre: "Comedy")
+
+    visit "/studios/#{fox.id}"
+
+    expect(page).to have_content(titanic.title)
+    expect(page).to have_content(dumbndumber.title)
+  end
 end
